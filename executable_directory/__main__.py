d80f9de18e1eb09450407b2d60a6d2d267abd08b
karanpalsinghshekhawat <karanpal609@gmail.com>
2021-12-24 07:44:43 +0530

    making a directory executable

---
+++ executable_directory/__main__.py
@@ -0,0 +1,7 @@
+"""
+This file is added to the directory
+which helps in making any directory executable.
+By executable, i mean that you can run this by calling python executable_directory
+"""
+
+print(" This directory is executable {}".format(__name__))
--- packages_first.py
+++ packages_first.py
@@ -1,2 +1,6 @@
 import pandas as pd
 import numpy as np
+
+from subpacakges import second
+
+print(__name__)
