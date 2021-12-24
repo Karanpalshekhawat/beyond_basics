96709bdf56e8489a1738b9f79924bad5ad2beee0
karanpalsinghshekhawat <karanpal609@gmail.com>
2021-12-24 11:40:00 +0530

    added readble class for __call__ dunder

--- callable_instance/resolver.py
+++ callable_instance/resolver.py
@@ -1,5 +1,14 @@
 """ Test how to use __call__"""
 
+
+
+
+
+"""
+parameters you pass in Init is used to start a class or instantiate a class
+while parameters you pass in 
+"""
+
 import socket
 
 class Resolver():
@@ -11,3 +20,21 @@ class Resolver():
         if host not in self._cache:
             self._cache[host] = socket.gethostbyname(host)
         return self._cache
+
+
+class Wages():
+
+    def __init__(self, hour_wage):
+        self.wage = hour_wage
+
+    def __call__(self, hour_work):
+        total_wage = self.wage * hour_work
+        return total_wage
+
+"""
+A way to call the Wages class
+
+wg = Wages(hourly wage)
+total_wage = wg(10)
+If number of working hours is same but hourly wage is different
+"""
