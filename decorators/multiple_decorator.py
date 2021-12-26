65d3f1b51c9976f328fdb3b946bfdcf8b76238cf
karanpalsinghshekhawat <karanpal609@gmail.com>
2021-12-26 14:15:50 +0530

    multiple decorator example

---
+++ decorators/multiple_decorator.py
@@ -0,0 +1,34 @@
+"""
+Callable return by the decorator 1
+is passed to decorator 2 and so on
+"""
+
+
+class Trace:
+
+    def __init__(self):
+        self.enabled = True
+
+    def __call__(self, f):
+
+        def wrap(*args, **kwargs):
+            if self.enabled:
+                print("Calling {}".format(f))
+                return f(*args, **kwargs)
+        return wrap
+
+tracer = Trace()
+
+
+def upper_case(f):
+    def wrap(*args, **kwargs):
+        x = f(*args, **kwargs)
+        return x.upper()
+    return wrap
+
+
+@tracer
+@upper_case
+def string_map(name):
+    return name
+
