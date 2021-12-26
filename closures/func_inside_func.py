70b7b6dff2d4c3cd5bed9ac38076fd2268201311
karanpalsinghshekhawat <karanpal609@gmail.com>
2021-12-26 11:21:14 +0530

    returning functions from inside function

--- closures/func_inside_func.py
+++ closures/func_inside_func.py
@@ -10,3 +10,19 @@ def sort_by_last(string):
 
 
 print(sort_by_last(['ab', 'aa', 'at', 'ad']))
+
+"""
+One important thing is you cannot call local function 
+as container_functions.local_function.
+Local functions are not members of containing function.
+"""
+
+
+def outer():
+    def inner():
+        print("local function")
+    return inner
+
+lf = outer()
+
+print(lf)
