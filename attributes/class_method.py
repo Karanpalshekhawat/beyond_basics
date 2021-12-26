10c4d2748ae428cb41796167adac81c73418b549
karanpalsinghshekhawat <karanpal609@gmail.com>
2021-12-26 19:39:00 +0530

    class methods details

---
+++ attributes/class_method.py
@@ -0,0 +1,29 @@
+"""
+Class method decorator,
+function that we use everytime we instantiate class
+"""
+
+
+class shipingcontainers:
+    next_serial = 1445
+
+    @classmethod
+    def _get_next_serial(cls):
+        # variable that we are using is class variables
+        result = cls.next_serial
+        cls.next_serial += 1
+        return result
+
+    def __init__(self, own, cont):
+        self.own = own
+        self.cont = cont
+        self.serial = shipingcontainers._get_next_serial()
+
+
+c1 = shipingcontainers("YML", "books")
+c2 = shipingcontainers("true", "story")
+
+print(shipingcontainers.next_serial)
+# value rite now of the class attribute
+print(c1.next_serial)
+print(c2.next_serial)
