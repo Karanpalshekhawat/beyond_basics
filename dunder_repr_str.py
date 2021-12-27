de1558cb9a9e6d246e8ddbd9caaf570dca6fd9e4
karanpalsinghshekhawat <karanpal609@gmail.com>
2021-12-27 18:45:47 +0530

    some more dunder examples

--- dunder_repr_str.py
+++ dunder_repr_str.py
@@ -29,6 +29,12 @@ class employee:
     def __str__(self):
         return ' Name is {} while email is {} '.format(self.name, self.email)
 
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
 
 emp1 = employee('Karan', 'Singh', 1000)
 emp2 = employee('Komal', 'Rathore', 2000)
@@ -38,3 +44,16 @@ print(repr(emp1))
 
 print(emp1.__str__())
 print(str(emp1))
+
+# if adding classes it will look into __add__ method inside the class
+# there are many methods for arithmatic
+print(emp1 + emp2)
+
+# length function on object, same output in below
+print('test_case'.__len__())
+print(len('test_case'))
+
+# lenght of fullname
+
+print(len(emp1))
+print(len(emp2))
