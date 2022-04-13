ab789aca12422e622383942408ee238d8165048b
karanpalsinghshekhawat <karanpal609@gmail.com>
2022-04-12 20:46:52 -0400

    full name and apply raise added

--- unit_testing/test_employee.py
+++ unit_testing/test_employee.py
@@ -11,5 +11,18 @@ class TestEmployee(unittest.TestCase):
         emp1.first = 'komal'
         self.assertEqual(emp1.email, 'komal.Shekhawat@email.com')
 
+    def test_fullname(self):
+        emp1 = employee.Employee('Karan', 'Shekhawat', 1000)
+        self.assertEqual(emp1.fullname, 'Karan Shekhawat')
+
+        emp1.first = 'komal'
+        self.assertEqual(emp1.fullname, 'komal Shekhawat')
+
+    def test_apply_raise(self):
+        emp1 = employee.Employee('Karan', 'Shekhawat', 1000)
+        emp1.apply_raise()
+        self.assertEqual(emp1.pay, 1050)
+
+
 if __name__ == '__main__':
     unittest.main()
